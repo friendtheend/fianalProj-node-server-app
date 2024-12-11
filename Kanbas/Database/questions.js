70e469c011e6fb59940124ac63648f5d8a35a77f
Keying <Keying01152000@gmail.com>
2024-12-11 15:27:50 -0800

    add new

--- Kanbas/Database/questions.js
+++ Kanbas/Database/questions.js
@@ -2,49 +2,60 @@ export default[
     {
       "_id": "1",
       "quizId": "1",
+      "pts":10,
       "name": "Introduction to Rocket Propulsion Question1",
       "description": "Basic principles of rocket propulsion and rocket engines.",
       "course": "RS101",
+      "title":"question title",
+      "type":"Multiple Choice",
       "answers": [
         {
-          "_id": "1",
-          "answerDescription": "Question1.",
-          "quizCourse": "M101"
-        },
-        {
-          "_id": "2",
-          "answerDescription": "Question2.",
-          "quizCourse": "M101"
-        },
-        {
-          "_id": "3",
-          "answerDescription": "Question3.",
-          "quizCourse": "M101"
-        }
+            id: 1, // 每个答案的唯一标识符（可用 index 或生成的唯一 ID）
+            text: "True", // 答案文本
+            isCorrect: true, // 是否为正确答案
+          },
+          {
+            id: 2,
+            text: "False",
+            isCorrect: false,
+          }
       ]
     },
 
     {
         "_id": "2",
         "quizId": "2",
+        "pts":10,
         "name": "Introduction to Rocket Propulsion",
         "description": "Basic principles of rocket propulsion and rocket engines.",
         "course": "RS101",
+        "title":"question title",
+        "type":"Multiple Choice",
         "answers": [
-          {
-            "_id": "1",
-            "answerDescription": "Question1.",
-            "quizCourse": "M101"
+        //   {
+        //     "_id": "1",
+        //     "answerDescription": "Question1.",
+        //     "quizCourse": "M101"
+        //   },
+        //   {
+        //     "_id": "2",
+        //     "answerDescription": "Question2.",
+        //     "quizCourse": "M101"
+        //   },
+        //   {
+        //     "_id": "3",
+        //     "answerDescription": "Question3.",
+        //     "quizCourse": "M101"
+        //   }
+        {
+            id: 1, // 每个答案的唯一标识符（可用 index 或生成的唯一 ID）
+            text: "True", // 答案文本
+            isCorrect: true, // 是否为正确答案
           },
           {
-            "_id": "2",
-            "answerDescription": "Question2.",
-            "quizCourse": "M101"
-          },
-          {
-            "_id": "3",
-            "answerDescription": "Question3.",
-            "quizCourse": "M101"
+            id: 2,
+            text: "False",
+            isCorrect: false,
           }
         ]
       },
